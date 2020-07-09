ab4f2759ce81f8a64d34317129f84a749dc20b9a
John Evans <john.wes.evans@gmail.com>
2020-07-09 18:12:07 -0500

    DEV Background fastload

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
-function onUnfinishedItemClicked() {
-  alert('Coming soon :)')
-}
+(() => {
+    const splash = document.querySelector('.splash-container');
+    splash.setAttribute('backgroundImage', 'url("../images/backgrodund.jpg")');
+})()
